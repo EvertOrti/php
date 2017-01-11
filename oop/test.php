eb469404c134d86b8219441bbc995f0442b3fd71
evert.orti <evert.orti@khk.ee>
2017-01-11 14:52:07 +0200

    text class initial version

--- oop/test.php
+++ oop/test.php
@@ -5,3 +5,11 @@
  * Date: 11.01.2017
  * Time: 14:47
  */
+// require the object creating and using class
+require_once('text.php');
+// create an object
+$sentence = new text();
+echo '<pre>';
+print_r($sentence);
+echo '</pre>';
+?>
