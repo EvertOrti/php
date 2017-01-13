b02008f65db66f14ba1417bd765f0d7c3ef89a7e
evert.orti <evert.orti@khk.ee>
2017-01-13 10:33:05 +0200

    Color text

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// index.php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 12.01.2017
+ * Time: 12:58
+ */
+// create and template object
+define('CLASSES_DIR', 'classes/');
+define('TMPL_DIR', 'tmpl/');
+require_once CLASSES_DIR.'template.php';
+// and use it
+// create an template object,
+// set up the file name for template
+// load template file content
+$tmpl = new template('main.html');
+// control the content of template object
+echo '<pre>';
+print_r($tmpl);
+echo '</pre>';
+?>
