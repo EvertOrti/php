780a1daf6d3a33fb393c02d7df7bde9619bbd97c
evert.orti <evert.orti@khk.ee>
2017-01-18 09:14:22 +0200

    Set up the pair - template element and template element real value

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -12,6 +12,16 @@ require_once 'http.php';
 class linkobject extends http
 {//class begin
     //class variables
-    var baseUrl = false; // base url
-    var protocol = 'http://'; // protocol for url - http
-}
+    var $baseUrl = false; // base url
+    var $protocol = 'http://'; // protocol for url - http
+    var $delim = '&amp;'; // & html tag
+    var $eq ='-'; // for url element pair element_name=element_value
+    //class methods
+    //construct
+    //create base url: http://xxx.xxx.xxx.xxx./path_to_file.php
+    function __construct() {
+        parent::__construct();
+        $this->baseURL = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    } //construct
+
+}//class end
