b60189d933656851a44f3f88a0294d13c7320be2
evert.orti <evert.orti@khk.ee>
2017-01-18 09:00:34 +0200

    Set up the pair - template element and template element real value

---
+++ classes/linkobject.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evert.orti
+ * Date: 18.01.2017
+ * Time: 8:48
+ */
+// only for testing
+// import http class
+require_once 'http.php';
+// only for testing
+class linkobject extends http
+{
+}
